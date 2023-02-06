22956848df3d2a3e1cd84bb4abb9e2eaf6e68fce
frparu <franpastorruiz19@gmail.com>
2023-02-06 19:03:50 +0100

    victor

--- test/cocheTest.php
+++ test/cocheTest.php
@@ -7,7 +7,7 @@ class cocheTest extends TestCase {
 
    public function testReposta() {
        
-      $objeto = new Coche("Ferrari", "petroleo", 100, 0);
+      $objeto = new Coche("Ferrari", "petroleo", 50, 0);
       $this->assertEquals(100, $objeto->reposta(50));
 
   }
